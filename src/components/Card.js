0bd22fd30a6b9586914d2f52dd83c28dcb0a4b29
eesaasheikh <eesaasheikh3@gmail.com>
2023-02-13 22:39:18 +0000

    new improvements to the card

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,6 +1,27 @@
 const Card = ({pokemon}) => {
 
 
+      // §§ Eesaa Code Test
+    //   function renderPokemon() {
+    //     const colors = {
+    //       fire: "#FFA6A6",
+    //       grass: "#A5F79E",
+    //       electric: "#FCF7DE",
+    //       water: "#B4DBFE",
+    //       ground: "#f4e7da",
+    //       rock: "#d5d5d4",
+    //       fairy: "#fceaff",
+    //       poison: "#C19FFD",
+    //       bug: "#f8d5a3",
+    //       dragon: "#97b3e6",
+    //       psychic: "#eaeda1",
+    //       flying: "#F5F5F5",
+    //       fighting: "#E6E0D4",
+    //       normal: "#F5F5F5"
+    //     };
+    //     const main_types = pokemon.type.name(colors);
+
+
     return ( 
         
         //         <div className= "meme" style = {backgroundImage: "https://assets.codepen.io/13471/sparkles.gif", opacity:"0.5"}></div>
@@ -15,8 +36,9 @@ const Card = ({pokemon}) => {
 
             {/* <img src={pokemon.imgUrl}/> */}
             
-            
-            <p className="pokiType">Type: {pokemon.type.name}</p>
+            <div className="pokiType">
+            <p >Type: {pokemon.type.name}</p>
+            </div>
             <div className="pokeInfo">
             <ul>
                 <li>HP: {pokemon.hp}</li>
@@ -27,10 +49,11 @@ const Card = ({pokemon}) => {
                 <li>Spe: {pokemon.speed}</li>
             </ul>
             </div>
-            <p>Rating: {pokemon.rating}</p>
+            <p className="rating">Rating: {pokemon.rating}</p>
         </div>
 
     );
 }
+
  
 export default Card;
--- src/containers/PokedexContainer.js
+++ src/containers/PokedexContainer.js
@@ -13,8 +13,31 @@ const PokedexContainer = () => {
         .then(data => setPokedex(data));
     }, []);
 
-    const pokedexList = pokedex.map((pokemon) => {
+    // §§ Eesaa Code Test
+    // function renderPokemon(pokedex) {
+    //     const colors = {
+    //       fire: "#FFA6A6",
+    //       grass: "#A5F79E",
+    //       electric: "#FCF7DE",
+    //       water: "#B4DBFE",
+    //       ground: "#f4e7da",
+    //       rock: "#d5d5d4",
+    //       fairy: "#fceaff",
+    //       poison: "#C19FFD",
+    //       bug: "#f8d5a3",
+    //       dragon: "#97b3e6",
+    //       psychic: "#eaeda1",
+    //       flying: "#F5F5F5",
+    //       fighting: "#E6E0D4",
+    //       normal: "#F5F5F5"
+    //     };
 
+    //     const main_types = pokedex.type.name(colors);
+
+       
+
+    // §§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§
+    const pokedexList = pokedex.map((pokemon) => {
         return (
             <>
                 <Card key={pokemon.id} pokemon={pokemon}/>
@@ -29,6 +52,7 @@ const PokedexContainer = () => {
             {console.log(pokedex)}
         </>
     );
+
 }
  
 export default PokedexContainer;
