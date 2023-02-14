0730ad2fe5913066f71b9eb28d571915cc1b97bb
Bavaharsan Nagarajah <bavaharsan.nagarajah@gmail.com>
2023-02-14 10:01:23 +0000

    put stats on bottom of card with hover effect

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,25 +1,40 @@
+import * as React from 'react';
+import Rating from '@mui/material/Rating';
+
 const Card = ({pokemon}) => {
 
 
     return ( 
-        <div className = "pokemonCard" style = {{backgroundImage : `url(${pokemon.imgUrl})`, width: "30vw", height: "45vh", backgroundSize: "cover", backgroundPosition:"center"}}>
+        <div className = "pokemonCard" style = {{backgroundImage : `url(${pokemon.imgUrl})`,
+                                                 width: "6.3cm",
+                                                 height: "8.8cm", 
+                                                 backgroundSize: "cover", 
+                                                 backgroundPosition:"center"}}>
+            <div className="cardBanner">
             <h2>{pokemon.name}</h2>
-
+            <Rating name="pokemonRating" size="small" className="cardRating" value={pokemon.rating} max={pokemon.rating} readOnly/>
+            </div>
             {/* <img src={pokemon.imgUrl}/> */}
             
             
             <p className="pokiType">Type: {pokemon.type.name}</p>
             <div className="pokeInfo">
-            <ul>
-                <li>HP: {pokemon.hp}</li>
-                <li>Atk: {pokemon.attack}</li>
-                <li>Def: {pokemon.defence}</li>
-                <li>SpA: {pokemon.specialAttack}</li>
-                <li>SpD: {pokemon.specialDefence}</li>
-                <li>Spe: {pokemon.speed}</li>
-            </ul>
+                <ul>
+                    <p><span className="statName">HP</span> {pokemon.hp}</p>
+                    <p><span className="statName">Atk</span> {pokemon.attack}</p>
+                
+                </ul>
+                <ul>
+                    <p><span className="statName">Def</span> {pokemon.defence}</p>
+                    <p><span className="statName">SpA</span> {pokemon.specialAttack}</p>
+
+                </ul>
+                <ul>
+                    <p><span className="statName">SpD</span> {pokemon.specialDefence}</p>
+                    <p><span className="statName">Spe</span> {pokemon.speed}</p>
+                </ul>
             </div>
-            <p>Rating: {pokemon.rating}</p>
+            {/* <p>Rating: {pokemon.rating}</p> */}
         </div>
     );
 }
